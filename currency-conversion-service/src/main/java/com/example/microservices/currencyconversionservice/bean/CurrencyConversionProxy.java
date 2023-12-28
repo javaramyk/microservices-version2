9dbd7a5c92ee2e49c7d2a6936f25843c671c9f06
karanara <karanara@mail.uc.edu>
2023-12-28 06:36:09 -0500

    created api gateway-connected to eureka

--- currency-conversion-service/src/main/java/com/example/microservices/currencyconversionservice/bean/CurrencyConversionProxy.java
+++ currency-conversion-service/src/main/java/com/example/microservices/currencyconversionservice/bean/CurrencyConversionProxy.java
@@ -4,8 +4,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-
-@FeignClient(name="currency-exchange",url="localhost:8000")
+//@FeignClient(name="currency-exchange",url="localhost:8000")
+@FeignClient(name="currency-exchange")
+//spring load balancer
 public interface CurrencyConversionProxy {
 	
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
