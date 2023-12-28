5a7d633bde8703178621cf4d5dfbb7c1e406f93d
karanara <karanara@mail.uc.edu>
2023-12-28 06:07:00 -0500

    created eureka server and updated accordingly to connect currency exchange and conversion services to eureka

---
+++ naming-server-eureka/src/test/java/com/example/microservices/namingservereureka/NamingServerEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservices.namingservereureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
