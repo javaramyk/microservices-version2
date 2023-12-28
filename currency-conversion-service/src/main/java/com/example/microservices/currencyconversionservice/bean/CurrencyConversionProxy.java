d9f713eafd2286150975232c6a9c7b3fe8e12923
karanara <karanara@mail.uc.edu>
2023-12-28 07:03:44 -0500

    enabled microservices to gateway

--- currency-conversion-service/src/main/java/com/example/microservices/currencyconversionservice/bean/CurrencyConversionProxy.java
+++ currency-conversion-service/src/main/java/com/example/microservices/currencyconversionservice/bean/CurrencyConversionProxy.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 //@FeignClient(name="currency-exchange",url="localhost:8000")
-@FeignClient(name="currency-exchange")
+@FeignClient(name="currency-exchange-service")
 //spring load balancer
 public interface CurrencyConversionProxy {
 	
