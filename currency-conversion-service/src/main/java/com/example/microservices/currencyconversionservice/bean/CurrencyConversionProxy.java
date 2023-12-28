ce9470034c06b038dfb1433347d58adfcfe6f75d
karanara <karanara@mail.uc.edu>
2023-12-28 05:32:03 -0500

    calling exchange microservice from currency conversion microservice using feign client

---
+++ currency-conversion-service/src/main/java/com/example/microservices/currencyconversionservice/bean/CurrencyConversionProxy.java
@@ -0,0 +1,13 @@
+package com.example.microservices.currencyconversionservice.bean;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+@FeignClient(name="currency-exchange",url="localhost:8000")
+public interface CurrencyConversionProxy {
+	
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public CurrencyConversion retrievalCurrency(@PathVariable String from,@PathVariable String to);
+}
--- currency-conversion-service/src/main/java/com/example/microservices/currencyconversionservice/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/example/microservices/currencyconversionservice/controller/CurrencyConversionController.java
@@ -3,6 +3,7 @@ package com.example.microservices.currencyconversionservice.controller;
 import java.math.BigDecimal;
 import java.util.HashMap;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,9 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.example.microservices.currencyconversionservice.bean.CurrencyConversion;
+import com.example.microservices.currencyconversionservice.bean.CurrencyConversionProxy;
 
 @RestController
 public class CurrencyConversionController {
+	
+	@Autowired
+	private CurrencyConversionProxy currencyProxy;
 
 	
 	@GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
@@ -31,5 +36,16 @@ public class CurrencyConversionController {
 				currencyConversion.getConversionMultiple(), 
 				quantity.multiply(currencyConversion.getConversionMultiple()), 
 				currencyConversion.getEnvironment()+ " " + "rest template");	
+	}
+	@GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversion retrieveCalculatedConversionValueFeign(@PathVariable String from,@PathVariable String to,
+			@PathVariable BigDecimal quantity) {
+		
+		CurrencyConversion currencyConversion = currencyProxy.retrievalCurrency(from, to);
+		return new CurrencyConversion(currencyConversion.getId(), 
+				from, to, quantity, 
+				currencyConversion.getConversionMultiple(), 
+				quantity.multiply(currencyConversion.getConversionMultiple()), 
+				currencyConversion.getEnvironment()+ " " + "feign Client");	
 	}
 }
